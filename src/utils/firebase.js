e65c4be5d0b1f2905df66f1786c46bf2b3fded98
Nithinsai2701 <nithincns2701@gmail.com>
2025-01-20 11:15:28 +0530

    Fixing the bugs

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBicTnwdsOnQ6CYWXR3Z7C2Pyo6M4MnPr0",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: "netflixgpt-28e0b.firebaseapp.com",
   projectId: "netflixgpt-28e0b",
   storageBucket: "netflixgpt-28e0b.firebasestorage.app",
   messagingSenderId: "363116441646",
-  appId: "1:363116441646:web:6933555b8b8f1efe1d6fbf",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-KBBFKRF2LR"
 };
 
